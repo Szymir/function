4bb6407155d5d0e71fa593a4b1f87c2c3989ead6
Szymon Malewski <malewskiszymon@gmail.com>
2018-02-19 17:17:31 +0100

    Change script.js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var triangle1Area = getTriangleArea(10, 15),
+	triangle2Area = getTriangleArea(3, 5),
+	triangle3Area = getTriangleArea(4, 2);
+
+function getTriangleArea(a, h) {
+	if (a>0 && h>0) {
+		return a*h/2;
+	} else {
+		console.log("Nieprawidłowe dane");
+	}
+}
+
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
