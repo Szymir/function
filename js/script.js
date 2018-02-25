7079c49b287cf30b65f9e3cd079d96413d7a1f7f
Szymon Malewski <malewskiszymon@gmail.com>
2018-02-25 12:45:44 +0100

    Change script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var someButtons = document.getElementsByClassName('button');
 var number = someButtons.length;
 
-for ( i=0; i < number ; i++) {
+for (var i=0; i < number; i++) {
 	alert(someButtons[i].innerHTML);
 }
